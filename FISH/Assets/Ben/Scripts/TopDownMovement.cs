418094be6b25ab8906a432d31ffbce5ced658342
MUKILTEO\1725203 <ben>
2026-01-22 10:22:59 -0800

    InputSystemStuff, made folder for Ben's scenes and scripts

---
+++ FISH/Assets/Ben/Scripts/TopDownMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TopDownMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        
+          {
+
+          }
+    }
+
+}
