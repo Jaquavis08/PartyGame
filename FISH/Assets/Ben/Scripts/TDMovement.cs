ddb17e766d8fb54c1f3f0405c68ce79d93f150d9
MUKILTEO\1725203 <ben>
2026-01-26 10:08:19 -0800

    polished movement and flip sprites

--- FISH/Assets/Ben/Scripts/TDMovement.cs
+++ FISH/Assets/Ben/Scripts/TDMovement.cs
@@ -8,20 +8,45 @@ public class TDMovement : MonoBehaviour
 
     private Rigidbody2D rb2d;
     private Vector2 _movementInput;
+    private Vector2 smoothedMovementInput;
+    private Vector2 movementInputSmoothVelocity;
+
+    private int facingDirection = 1;
 
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+               Flip();
+    }
+
     private void FixedUpdate()
     {
-        rb2d.linearVelocity = _movementInput * speed;
+        smoothedMovementInput = Vector2.SmoothDamp(smoothedMovementInput, _movementInput, ref movementInputSmoothVelocity, 0.35f);
+        rb2d.linearVelocity = smoothedMovementInput * speed;
+    }
+
+    public void Flip()
+    {
+        if (smoothedMovementInput.x > 0.1f)
+        {
+            facingDirection = 1;
+        }
+        else if(smoothedMovementInput.x < -0.1f)
+        {
+            facingDirection = -1;
+        }
+       
+      transform.localScale = new Vector3(1, facingDirection, 1);
     }
 
     private void OnMove(InputValue inputValue)
     {
         _movementInput = inputValue.Get<Vector2>();
     }
+
 }
 
