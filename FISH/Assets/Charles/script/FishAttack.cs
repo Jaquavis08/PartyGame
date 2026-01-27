a4f01528458849b9bb455c681f10c9f961a657b4
MUKILTEO\1725289 <charles82508@gmail.com>
2026-01-27 09:32:41 -0800

    i dont know if it works

---
+++ FISH/Assets/Charles/script/FishAttack.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FishAttack : MonoBehaviour
+{
+    public float attackRange = 1.0f;
+    public Rigidbody2D rb2d;
+    public void Attack()
+    {
+        PlayerInput pI = GetComponent<PlayerInput>();
+        GetComponent <Animator>().SetTrigger("Attack");
+        
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInput pI = GetComponent<PlayerInput>();
+        if (PlayerInput.all[pI.playerIndex].actions["Attack"].WasPerformedThisFrame())
+        {
+            Vector2 attackDirection = new Vector2(transform.localScale.x, 0).normalized;
+            RaycastHit2D hit = Physics2D.Raycast(rb2d.position, attackDirection, attackRange, LayerMask.GetMask("Player"));
+            if (hit.collider != null)
+            {
+                Debug.Log("Hit " + hit.collider.name);
+                // Apply damage or effects to the hit player
+            }
+        }
+        Gizmos.DrawWireSphere(rb2d.position + new Vector2(transform.localScale.x * attackRange, 0), 0.1f);
+    }
+}
