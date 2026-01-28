6bd071b023cce83caf3acce619f38dae76e4943d
MUKILTEO\1725289 <charles82508@gmail.com>
2026-01-28 09:29:49 -0800

    it almoast works

--- FISH/Assets/Charles/script/FishAttack.cs
+++ FISH/Assets/Charles/script/FishAttack.cs
@@ -1,36 +1,57 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput), typeof(Animator), typeof(Rigidbody2D))]
 public class FishAttack : MonoBehaviour
 {
     public float attackRange = 300.0f;
     public Rigidbody2D rb2d;
+
+    private PlayerInput playerInput;
+    private Animator animator;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+       // animator = GetComponent<Animator>();
+        if (rb2d == null) rb2d = GetComponent<Rigidbody2D>();
+    }
+
     public void Attack()
     {
-        PlayerInput pI = GetComponent<PlayerInput>();
-        GetComponent <Animator>().SetTrigger("Attack");
-        
+        animator.SetTrigger("Attack");
     }
 
     void Update()
     {
-        PlayerInput pI = GetComponent<PlayerInput>();
-        if (PlayerInput.all[pI.playerIndex].actions["Attack"].WasPerformedThisFrame())
+        if (playerInput == null || rb2d == null) return;
+
+        // Use the PlayerInput instance actions directly
+        var attackAction = playerInput.actions["Attack"];
+        if (attackAction != null && attackAction.WasPerformedThisFrame())
         {
-            Vector2 attackDirection = new Vector2(transform.localScale.x, 0).normalized;
+            Vector2 attackDirection = new Vector2(Mathf.Sign(transform.localScale.x), 0f).normalized;
             RaycastHit2D hit = Physics2D.Raycast(rb2d.position, attackDirection, attackRange, LayerMask.GetMask("Player"));
             if (hit.collider != null)
             {
-                Debug.Log("Hit");
+                Debug.Log("Hit: " + hit.collider.name);
                 // Apply damage or effects to the hit player
 
             }
         }
-        Gizmos.DrawWireSphere(rb2d.position + new Vector2(transform.localScale.x * attackRange, 0), 0.1f);
-    
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (rb2d == null) return;
+        Vector2 spherePos = rb2d.position + new Vector2(Mathf.Sign(transform.localScale.x) * attackRange, 0f);
+        Gizmos.DrawWireSphere(spherePos, 0.1f);
+    }
+
     private void OnAttack(InputValue inputValue)
     {
-        Attack();
+        // Use the InputValue properly instead of leaving a stray expression
+        if (inputValue.Get<float>() > 0f)
+            Attack();
     }
 }
