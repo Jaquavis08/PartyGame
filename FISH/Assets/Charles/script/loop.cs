44425c49569c6c9562493e47007762d3be8ab85a
MUKILTEO\1725289 <charles82508@gmail.com>
2026-01-29 08:48:40 -0800

    it still dont work

--- FISH/Assets/Charles/script/loop.cs
+++ FISH/Assets/Charles/script/loop.cs
@@ -1,27 +1,46 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class loop : MonoBehaviour
 {
+    public GameObject borderTop;
+    public GameObject borderBottom;
+    public GameObject borderLeft;
+    public GameObject borderRight;
     private GameObject player;
+    public Rigidbody2D rb2d;
+ 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player.transform.position.y >= 505)
-        {
-            Vector2 newPosition = new Vector2(player.transform.position.x, 5);
-            player.transform.position = newPosition;
-        }
-        if (player.transform.position.y <= 0)
-        {
-            Vector2 newPosition = new Vector2(player.transform.position.x, 500);
-            player.transform.position = newPosition;
-        }
+       
 
     }
+    private void OnTriggerCollider2D(Collider2D other)  
+    {
+        if (other.CompareTag("borderTop"))
+        {
+           player.transform.position = new Vector3(player.transform.position.x, borderBottom.transform.position.y + 1f, player.transform.position.z);
+        }
+        if (other.CompareTag("borderBottom"))
+        {
+            player.transform.position = new Vector3(player.transform.position.x, borderTop.transform.position.y - 1f, player.transform.position.z);
+        }
+        if (other.CompareTag("borderLeft"))
+        {
+            player.transform.position = new Vector3(borderRight.transform.position.x - 1f, player.transform.position.y, player.transform.position.z);
+        }
+        if (other.CompareTag("borderRight"))
+        {
+            player.transform.position = new Vector3(borderLeft.transform.position.x + 1f, player.transform.position.y, player.transform.position.z);
+        }
+    }
+
 }
