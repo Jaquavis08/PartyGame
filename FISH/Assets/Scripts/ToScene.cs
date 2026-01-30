9e090181ca02a5a4708bbd47840221adca56a294
MUKILTEO\1720941 <1720941>
2026-01-30 08:40:54 -0800

    to scene

--- FISH/Assets/Scripts/ToScene.cs
+++ FISH/Assets/Scripts/ToScene.cs
@@ -1,9 +1,13 @@
 using System.Collections;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ToScene : MonoBehaviour
 {
+    private Button button1;
+
+
     public void LoadSceneByName(string sceneName)
     {
         StartCoroutine(LoadSceneAfterDelay(sceneName, 3f));
@@ -13,5 +17,27 @@ public class ToScene : MonoBehaviour
     {
         yield return new WaitForSeconds(delay);
         SceneManager.LoadScene(sceneName);
+    }
+
+    //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    void Start()
+    {
+        button1 = transform.Find("Exit").GetComponent<Button>();
+        button1.onClick.AddListener(() =>
+        {
+            Quit1();
+        });
+    }
+
+    public void Quit1()
+    {
+      StartCoroutine(QuitAfterDelay(0, 3f));
+    }
+
+    public IEnumerator QuitAfterDelay(int quit, float delays)
+    {
+        yield return new WaitForSeconds(delays);
+        Application.Quit(quit);
     }
 }
