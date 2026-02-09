88c6c41e6ac32fae6de8dbbe6f6055a5352ee286
MUKILTEO\1725289 <charles82508@gmail.com>
2026-02-09 09:40:46 -0800

    it dont pickup

--- FISH/Assets/Charles/script/AttackArea.cs
+++ FISH/Assets/Charles/script/AttackArea.cs
@@ -1,38 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class AttackArea : MonoBehaviour
 {
-
     private bool isHit = false;
     public bool IsHit => isHit;
-    private FishAttack  fishAttack;
-
-    public void ClearHit() => isHit = false;
-
-    void Start()
-    {
-        // Adjust to where AttackArea lives (same object, child, or parent)
-        fishAttack = GetComponentInParent<FishAttack>();
-        if (fishAttack == null)
-            Debug.LogWarning("AttackArea not found on this GameObject or its parent.");
-    }
-   
-    void Update()
-    {
-        if (fishAttack != null && fishAttack.IsAttack)
-        {
-            Debug.Log("Detected hit from AttackArea - handle it here.");
-            // Handle hit once, then clear the flag:
-            fishAttack.clearattacking();
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("hit") && fishAttack.IsAttack == true)
+        if (other.gameObject.CompareTag("hit"))
         {
-            Debug.Log("Hit");
+            Debug.Log(" true Hit");
             isHit = true;
+            var scaleDrop = other.GetComponentInChildren<ScaleDrop>();
+            if (scaleDrop != null)
+            {
+                // Use a public trigger method instead of calling a hidden StartCoroutine overload
+                scaleDrop.TriggerDrop();
+            }
         }
     }
 }
