1e6fbd4965d25e8ffdc0771d385fc7d02e57dcc4
MUKILTEO\1725289 <charles82508@gmail.com>
2026-02-10 09:06:08 -0800

    MACRION&CHEESE

--- FISH/Assets/Charles/script/ScalePickup.cs
+++ FISH/Assets/Charles/script/ScalePickup.cs
@@ -7,6 +7,7 @@ public class ScalePickup : MonoBehaviour
     public GameObject scale;
     public GameObject playerscore;
     public float ps;
+    public Rigidbody2D rb2d;
 
     void Start()
     {
@@ -36,8 +37,9 @@ public class ScalePickup : MonoBehaviour
             if (scale != null)
             {
                 Debug.Log("scale colected ");
-                scale.SetActive(false);// Destroy the pickup after collectin
-               // scale.transform.position = new Vector3(hit.x, hit.y, hit.z); // Move it far away to effectively "destroy" it
+                scale.SetActive(false);
+               // Destroy the pickup after collectin
+                scale.transform.position = new Vector3(parent, hit.y, hit.z); // Move it far away to effectively "destroy" it
             }
         }
     }
