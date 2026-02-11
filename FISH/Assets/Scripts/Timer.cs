5e0bc7b6596018dbf26cd8659bad8dbfc62c5132
MUKILTEO\1725489 <kayla.gerlach42@gmail.com>
2026-02-11 10:23:07 -0800

    tank shelf animation thing

---
+++ FISH/Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Slider timerSlider;
+    public float gameTime;
+    public Animator zoomOut;
+    public Image tankShelf;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timerSlider.maxValue = gameTime;
+        timerSlider.value = gameTime;
+        tankShelf.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = gameTime - Time.time;
+        if(time <= 0.001)
+        {
+            Debug.Log("Time's up!");
+            tankShelf.enabled = true;
+            zoomOut.SetTrigger("out");
+
+        }
+        else
+        {
+            timerSlider.value = time;
+        }
+    }
+}
